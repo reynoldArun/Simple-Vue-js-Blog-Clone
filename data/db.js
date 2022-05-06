a89f488763a32a4879607f632d0c54e48d51a2b3
rey <reynold.seq@gmail.com>
2022-05-06 07:23:45 +0530

    images

---
+++ data/db.js
@@ -0,0 +1,239 @@
+import image1 from '../src/assets/pan.jpg'
+import image2 from '../src/assets/1.jpg'
+import image3 from '../src/assets/2.jpg'
+import image4 from '../src/assets/3.jpg'
+import image5 from '../src/assets/4.jpg'
+import image6 from '../src/assets/5.jpg'
+import image7 from '../src/assets/6.jpg'
+
+const data = {
+    recipes: [
+        {
+            slug: "Good Old Fashioned Pancakes",
+            title: "Good Old Fashioned Pancakes",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image1,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },
+        {
+            slug: "Sandwich",
+            title: "Sandwich",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image2,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },{
+            slug: "French Fires",
+            title: "French Fires",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image3,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },
+        {
+            slug: "Tandoori Chicken",
+            title: "Tandoori Chicken",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image4,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },
+        {
+            slug: "Good Old Fashioned Pancakes",
+            title: "Good Old Fashioned Pancakes",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image5,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },
+        {
+            slug: "Hakka Noodles",
+            title: "Hakka Noodles",
+            ratings: "16,748",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image6,
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        },
+        {
+            slug: "Cup Cakes",
+            title: "Cup Cakes",
+            ratings: "16,7481",
+            reviews: "13218",
+            description: "This is a great recipe that I found in my Grandma's recipe book. Judging from the weathered look of this recipe card, this was a family favorite. ",
+            writer: "Jane Doe",
+            img: image7,
+            cat: "Veg",
+            ingredients: [
+                "1 ½ cups all-purpose flour",
+                "3 ½ teaspoons baking powder",
+                "¼ teaspoon salt, or more to taste",
+                "1 tablespoon white sugar",
+                "1 ¼ cups milk",
+                "1/2 red onion, chopped",
+                "1 egg",
+                "3 tablespoons butter, melted ",
+              ],
+              Directions: [
+                  "In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+                  "Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot."
+              ],
+              timeTaken: [
+                  {
+                      "cook": "15mins",
+                      "total": "20mins",
+                      "Prep": "5mins",
+                      "Servings": "8",
+                      "Yield": "8 servings"
+                  }
+              ]
+        }
+    ]
+}
+
+
+export default data
